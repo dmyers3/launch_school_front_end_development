9a3cb6ab3f7f8f780d5142b3e3ad5e0c50808ce9
Daniel Myers <daniel.p.myers@gmail.com>
2017-03-03 03:21:33 +0000

    fix main page to stay current after editing todo

--- 220/Contact Manager/contacts.js
+++ 220/Contact Manager/contacts.js
@@ -11,7 +11,6 @@
   
 //   media breakpoint at 768px to go one column for contacts and addcontact/search
   
- 
 
 
 $(function() {
@@ -21,58 +20,62 @@ $(function() {
   
   Handlebars.registerPartial('contactTemplate', $('#contact_template').html());
   
-  // var contact1 = { name: "Dan", phone: '555-6666', email: 'danny@gmail.com' }
-  // var contact2 = { name: "Katie", phone: '555-8888', email: 'katie@gmail.com' }
+  var contacts = getContacts() || [];
+  var noContactsHTML = '<div class="no_contacts"><p>There are no contacts.</p><a class="button add" href="#">Add Contact</a></div>';
   
-  var contacts = [];
-  var numContacts = contacts.length;
-  var hiddenHTML = '';
-  
-  var slideHTML = $('<div />').append($('.slide').clone()).html();
-  var mainHeaderHTML = $('<div />').append($('.main_header').clone()).html();
-  var mainContainerHTML = $('<div />').append($('.main_container').clone()).html();
-  console.log(slideHTML);
+  displayContacts();
   
   var Contact = {
-    init: function(name, phone, email) {
+    init: function(name, phone, email, tags) {
       this.name = name;
       this.phone = phone;
       this.email = email;
+      this.tags = tags;
       return this;
     }
   };
   
-  // function addButtonEventListeners() {
-  //   $('button .submit').on('submit', function(e) {
-  //     e.preventDefault();
-  //     var name = $('#name').val();
-  //     var phone = $('#phone').val();
-  //     var email = $('#email').val();
-  //     contacts.push( Object.create(Contact).init(name, phone, email));
-  //     $('form').slideUp();
-  //     $('.main_container').html(contactsList({ contacts: contacts}));
-  //   });
-    
-  //   $('.cancel').on('click', function(e) {
-  //     e.preventDefault();
-  //     console.log(hiddenHTML);
-  //     $('form').slideUp(500);
-  //     $('main').append(hiddenHTML);
-  //     // $('main').html(contactsList({ contacts: contacts}));
-  //   });
-  // }
+  function setContacts(contacts) {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }
   
-  function addSubmitListener() {
+  function getContacts() {
+    return JSON.parse(localStorage.getItem('contacts'));
+  }
+  
+  function displayContacts() {
+    if (contacts.length > 0) {
+      $('.contacts_container').html(contactsList({ contacts: getContacts()}));
+    } else {
+      $('.contacts_container').html(noContactsHTML);
+    }
+    $('.display_all').hide();
+  }
+  
+  function addSubmitListener(contact) {
      $('form').on('submit', function(e) {
       e.preventDefault();
       var name = $('#name').val();
       var phone = $('#phone').val();
       var email = $('#email').val();
-      var sample = Object.create(Contact);
-      contacts.push( Object.create(Contact).init(name, phone, email));
-      console.log(sample);
+      var tags = $('#tags').val().split(',').map(function(tag) {
+        return tag.trim();
+      });
+      
+      // Edit contact
+      if (contact) {
+        contact.name = name;
+        contact.phone = phone;
+        contact.email = email;
+        contact.tags = tags;
+        // Add new contact
+      } else {
+        contacts.push( Object.create(Contact).init(name, phone, email, tags));
+      }
+      // Update contacts in local storage
+      setContacts(contacts);
       $('form').slideUp();
-      $('.main_container').html(contactsList({ contacts: contacts}));
+      displayContacts();
       $('.slide').slideDown();
       $('form').off('submit');
     });
@@ -87,53 +90,66 @@ $(function() {
   
   $('main').on('click', '.edit', function(e) {
     e.preventDefault();
-    $('.no_contacts').slideUp();
-    $('.contacts').slideUp();
-    addSubmitListener();
+    $('.slide').slideUp(500);
+    // finds email of contact we want to edit and then searches through contacts
+    // array to find contact with matching email. Uses that contact to populate
+    // edit form
+    var email = $(e.target).closest('.contact').find('.email').text();
+    var matchingContact = contacts.filter(function(contact) {
+      return contact.email === email;
+    })[0];
+    $('.add_edit_form').html(addEditTemplate({ type: 'Edit', name: matchingContact.name, email: matchingContact.email, phone: matchingContact.phone, tags: matchingContact.tags.join(', ')}));
+    addSubmitListener(matchingContact);
   });
   
   $('main').on('click', '.delete', function(e) {
     e.preventDefault();
-    $('.no_contacts').slideUp();
-    $('.contacts').slideUp();
+    if (confirm('Are you sure you want to delete this contact?')) {
+      // find matching contact index by searching email in contacts array and then 
+      // deletes that contact by splice
+      var email = $(e.target).closest('.contact').find('.email').text();
+      var matchingContactIndex = contacts.findIndex(function(contact) {
+        return contact.email === email;
+      });
+      contacts.splice(matchingContactIndex, 1);
+      setContacts(contacts);
+      // redisplay contacts list if there still are contacts, if not display
+      // empty contacts HTML
+      displayContacts();
+    }
   });
-  
-   
     
   $('main').on('click', '.cancel', function(e) {
     e.preventDefault();
     $('.slide').slideDown(500);
     $('form').slideUp(500);
-    
-    // if (numContacts === 0) {
-    //   $('main').append(initalMainHTML);
-    // } else {
-    //     $('main').append(contactsList({ contacts: contacts}));
-    // }
     $('form').off('submit');
   });
   
-  // $('.add').on('click', function(e) {
-  //   e.preventDefault();
-  //   hiddenHTML = $('.slide').html();
-  //   console.log(hiddenHTML);
-  //   $('.slide').slideUp(500);
-  //   $('main').append(addEditTemplate({ type: 'Create', name: '', email: '', phone: ''}));
-  //   addButtonEventListeners();
-  // });
+   $('main').on('click', '.tag', function(e) {
+    e.preventDefault();
+    var tag = e.target.innerHTML;
+    var matchingContacts = contacts.filter(function(contact) {
+      return contact.tags.indexOf(tag) >= 0;
+    })
+    $('.contacts_container').html(contactsList({ contacts: matchingContacts}));
+    $('.display_all').show();
+  });
   
-  // $('.edit').on('click', function(e) {
-  //   e.preventDefault();
-  //   $('.no_contacts').slideUp();
-  //   $('.contacts').slideUp();
-  // });
+  $('main').on('click', '.display_all_link', function(e) {
+    e.preventDefault();
+    displayContacts();
+  });
   
-  // $('.delete').on('click', function(e) {
-  //   e.preventDefault();
-  //   $('.no_contacts').slideUp();
-  //   $('.contacts').slideUp();
-  // });
- 
+  $('#search').on('keyup', function(e) {
+    var search = ($('#search').val());
+    var match = new RegExp(search, "gi");
+    var matchingContacts = contacts.filter(function(contact) {
+      return contact.name.match(match);
+    })
+    $('.contacts_container').html(contactsList({ contacts: matchingContacts}));
+  })
+  
 })
 
 // start with header and no contacts
